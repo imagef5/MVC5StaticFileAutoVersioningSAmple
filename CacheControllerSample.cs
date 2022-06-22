10ba241a1a98db1553ebb61526ffd0501f2e51d1
Younchang Heo <imagef5@naver.com>
2022-06-22 14:19:27 +0900

    Create CacheControllerSample.cs

---
+++ CacheControllerSample.cs
@@ -0,0 +1,34 @@
+using LCTM.Utils;
+using System.Web.Mvc;
+
+namespace MVC5AutoVersioningSample.Controllers
+{
+    public class CacheController : Controller
+    {
+        // GET: Cashe
+        public ActionResult SetCache(string key, string value)
+        {
+            CacheUtil.SetSignaledCash(key, value);
+            return Content($"{key} 에 {value} 가 설정 되었습니다.");
+        }
+
+        public ActionResult GetCache(string key)
+        {
+            var value = CacheUtil.GetSignaledCash(key);
+
+            return Content("value : " + value?.ToString());
+        }
+
+        public ActionResult FlushCache(string key)
+        {
+            CacheUtil.FlushSignaledCash(key);
+            return Content($"{key} 값이 삭제 되었습니다.");
+        }
+
+        public ActionResult AllFlushCache()
+        {
+            CacheUtil.AllFlushSignaledCash();
+            return Content("모든 캐시가 삭제 되었습니다.");
+        }
+    }
+}
